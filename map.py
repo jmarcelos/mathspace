1caa76d886d7604c74dc18b3e2fa0924b3974fd7
Joao Sgreccia <jmarcelos@gmail.com>
2017-10-29 18:51:00 -0200

    splitting implementation

--- map.py
+++ map.py
@@ -4,7 +4,41 @@ class Map:
 
     MAX_COST = float('inf')
 
+
     def __init__(self, str_map):
+
+        self._validation(str_map)
+
+        paths = str_map.split()
+        size_matrix = sqrt(len(paths)) 
+        self.grid = []
+        line = []
+        
+        for index, path in enumerate(paths):
+            if index != 0 and index % size_matrix == 0:
+                self.grid.append(line)
+                line = []
+            line.append(int(path, 16))
+
+        self.grid.append(line)
+        self.start_x, self.start_y = 0, 0
+        self.end_x, self.end_y = len(self.grid) - 1, len(self.grid) - 1
+
+    def get_position_value(self, position_x, position_y):
+
+        if not self.is_inside_grid(position_x, position_y):
+            return self.MAX_COST
+        return self.grid[position_x][position_y]
+
+    def is_inside_grid(self, position_x, position_y):
+
+        if position_x < self.start_x or position_x > self.end_x:
+            return False
+        if position_y < self.start_y or position_y > self.end_y:
+            return False
+        return True
+
+    def _validation(self, str_map):
 
         if not str_map or not isinstance(str_map, str):
             raise ValueError("A valid str map should be provided")
@@ -15,37 +49,3 @@ class Map:
         #assuming a square grid
         if round(size_matrix) != size_matrix:
             raise ValueError("Invalid map generation")
-
-        self.grid = []
-        line = []
-        
-        for index, path in enumerate(paths):
-            if index != 0 and index % size_matrix == 0:
-                self.grid.append(line)
-                line = []
-            line.append(int(path, 16))
-        self.grid.append(line)
-        self.start_x, self.start_y = 0, 0
-        self.end_x, self.end_y = len(self.grid) - 1, len(self.grid) - 1
-
-    def _split_line(line):
-    
-        line = []
-        for val in line:
-            line.append(int(val, 16))
-        return line
-
-    def get_position_value(self, position_x, position_y):
-
-        if not self.is_inside_grid(position_x, position_y):
-            return self.MAX_COST
-        return self.grid[position_x][position_y]
-
-
-    def is_inside_grid(self, position_x, position_y):
-
-        if position_x < self.start_x or position_x > self.end_x:
-            return False
-        if position_y < self.start_y or position_y > self.end_y:
-            return False
-        return True
--- robot.py
+++ robot.py
@@ -13,14 +13,21 @@ class Robot:
         self.end_x = end[0]
         self.start_y = start[1]
         self.end_y = end[1]
+        self.algorithms = NaiveImplementation(maps, self.end_x, self.end_y)
 
     def get_min_cost_path(self):
 
         path = []
         visited = []
-        value = self.find_least_cost_path(self.start_x, self.start_y, visited)
-        return (value[0], value[1])
+        value = self.algorithms.find_least_cost_path(self.start_x, self.start_y, visited)
+        return (value[0], list(reversed((value[1]))))
 
+class NaiveImplementation:
+
+    def __init__(self, maps, end_x, end_y):
+        self.maps = maps
+        self.end_x = end_x
+        self.end_y = end_y
 
     def find_least_cost_path(self, position_x, position_y, visited):
 
@@ -33,17 +40,16 @@ class Robot:
         visited.append((position_x, position_y))
         if position_x == self.end_x and position_y == self.end_y:
             return self.maps.get_position_value(position_x, position_y), []
-        
-        
+
         cost_right, cost_left, cost_up, cost_down = (self.maps.MAX_COST, []),(self.maps.MAX_COST, []), (self.maps.MAX_COST, []),(self.maps.MAX_COST, [])
         cost_down = self.find_least_cost_path(position_x+1, position_y, deepcopy(visited))
         cost_down[1].append("D")
-        cost_left = self.find_least_cost_path(position_x-1, position_y, deepcopy(visited))
-        cost_left[1].append("L")
+        #cost_left = self.find_least_cost_path(position_x-1, position_y, deepcopy(visited))
+        #cost_left[1].append("L")
         cost_right = self.find_least_cost_path(position_x, position_y+1, deepcopy(visited))
         cost_right[1].append("R")
-        cost_up = self.find_least_cost_path(position_x, position_y-1, deepcopy(visited))
-        cost_up[1].append("U")
+        #cost_up = self.find_least_cost_path(position_x, position_y-1, deepcopy(visited))
+        #cost_up[1].append("U")
         
         min_cost = self.get_least_cost_step(cost_right, cost_left, cost_up, cost_down)
         actual_cost = self.maps.get_position_value(position_x, position_y)
@@ -52,6 +58,3 @@ class Robot:
 
     def get_least_cost_step(self, cost_right, cost_left, cost_up, cost_down):
         return min(cost_right, cost_left, cost_up, cost_down)
-
-class NaiveLeastCostPath
-
